614e99793cd27440e8f8bc4a793b9588a266f396
dcflachs <dcflachs@gmail.com>
2019-12-30 18:12:06 -0500

    Filling in the scripts a bit.

--- build_in_docker.sh
+++ build_in_docker.sh
@@ -1,1 +1,3 @@
 #!/bin/bash
+
+exec docker run --rm --tmpfs /tmp -v ./archive:/mnt/output:rw -e OUTPUT_FOLDER="/mnt/output" -v ./source:/mnt/source:ro TODO_IMAGE_NAME /mnt/output/pkg_build.sh
--- source/pkg_build.sh
+++ source/pkg_build.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 [ -z "$OUTPUT_FOLDER" ] && echo "Output Folder not set" && exit 1
 tmpdir=/tmp/tmp.$(( $RANDOM * 19318203981230 + 40 ))
-
+KATA_VERSION="${KATA_VERSION:-1.9.3}"
+KATA_RELEASE_TAR=kata-static-$KATA_VERSION-x86_64.tar.xz
 
 mkdir -p $tmpdir
 
 cd $tmpdir
+
+wget https://github.com/kata-containers/runtime/releases/download/$KATA_VERSION/$KATA_RELEASE_TAR
+
+tar xzvf $KATA_RELEASE_TAR
+
 #makepkg -l y -c y ${archive}/${plugin}-${version}-x86_64-1.txz
 rm -rf $tmpdir
