c9bf68d930a8105afb80497a6752324d93b54ff0
dcflachs <dcflachs@gmail.com>
2020-05-19 23:00:50 -0400

    Moved kata.runtime pkg_build.sh to subdirectory of source

--- build_in_docker.sh
+++ build_in_docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-exec docker run --rm --tmpfs /tmp -v $PWD/archive:/mnt/output:rw -e OUTPUT_FOLDER="/mnt/output" -v $PWD/source:/mnt/source:ro vbatts/slackware-base:latest /mnt/source/pkg_build.sh
+exec docker run --rm --tmpfs /tmp -v $PWD/archive:/mnt/output:rw -e OUTPUT_FOLDER="/mnt/output" -v $PWD/source:/mnt/source:ro vbatts/slackware-base:latest /mnt/source/kata.runtime/pkg_build.sh
