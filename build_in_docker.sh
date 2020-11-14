7e3c4f6e10e927feb9efa93c75192047ed84c7c3
dcflachs <dcflachs@gmail.com>
2020-11-14 15:39:45 -0500

    Passing $UI_VERSION_LETTER env var to ui package build

--- build_in_docker.sh
+++ build_in_docker.sh
@@ -3,5 +3,5 @@
 if getopts "u" arg; then
 docker run --rm --tmpfs /tmp -v $PWD/archive:/mnt/output:rw -e OUTPUT_FOLDER="/mnt/output" -e KATA_VERSION -v $PWD/source/kata.runtime.ui:/mnt/source:ro vbatts/slackware:latest /mnt/source/pkg_build.sh
 else
-docker run --rm --tmpfs /tmp -v $PWD/archive:/mnt/output:rw -e OUTPUT_FOLDER="/mnt/output" -e KATA_VERSION -v $PWD/source/kata.runtime:/mnt/source:ro vbatts/slackware:latest /mnt/source/pkg_build.sh
+docker run --rm --tmpfs /tmp -v $PWD/archive:/mnt/output:rw -e OUTPUT_FOLDER="/mnt/output" -e KATA_VERSION -v $PWD/source/kata.runtime:/mnt/source:ro vbatts/slackware:latest /mnt/source/pkg_build.sh $UI_VERSION_LETTER
 fi
